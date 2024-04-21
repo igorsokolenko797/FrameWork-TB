cf2d85730a083064d3da55a20d381366e324bca8
igorsokolenko797 <58339058+igorsokolenko797@users.noreply.github.com>
2024-04-21 17:40:14 +0400

    Add files via upload

---
+++ app/keyboard.py
@@ -0,0 +1,32 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton)
+from aiogram.types.web_app_info import WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from app.database.requests import get_categories, get_category_item
+
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог'),
+                                      KeyboardButton(text='Открыть веб страницу', web_app=WebAppInfo(
+                                          url='https://igorsokolenko797.github.io/FrameWork-WebSite/'))],
+                                     [KeyboardButton(text='Корзина')],
+                                     [KeyboardButton(text='Контакты'),
+                                      KeyboardButton(text='О нас')]],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите пункт меню...')
+
+
+async def categories():
+    all_categories = await get_categories()
+    keyboard = InlineKeyboardBuilder()
+    for category in all_categories:
+        keyboard.add(InlineKeyboardButton(text=category.name, callback_data=f"category_{category.id}"))
+    keyboard.add(InlineKeyboardButton(text='На главную', callback_data='to_main'))
+    return keyboard.adjust(2).as_markup()
+
+
+async def items(category_id):
+    all_items = await get_category_item(category_id)
+    keyboard = InlineKeyboardBuilder()
+    for item in all_items:
+        keyboard.add(InlineKeyboardButton(text=item.name, callback_data=f"item_{item.id}"))
+    keyboard.add(InlineKeyboardButton(text='На главную', callback_data='to_main'))
+    return keyboard.adjust(2).as_markup()
